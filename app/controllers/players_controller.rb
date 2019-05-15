62ee4d497f8673e36ac85d3c1a42e869a5c6b737
Ben Muller <ben.k.muller@gmail.com>
2019-05-15 10:11:13 +1000

    debug

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,12 +3,14 @@ class PlayersController < ApplicationController
     player = Player.new(player_params)
 
     game = Game.find(player_params[:game_id])
-    
+
     if player.save
       serialized_data = ActiveModelSerializers::Adapter::Json.new(
         PlayerSerializer.new(player)
       ).serializable_hash
-      PlayersChannel.broadcast_to game, serialized_data
+      PlayersChannel.broadcast_to game, "FUCK YOU"
+      
+      #serialized_data
       head :ok
     end
   end
